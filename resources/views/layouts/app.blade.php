5b8ee37de6cbbb8df0bff4926b419bfd44b9f060
odettepaulo <53255686+odettepaulo@users.noreply.github.com>
2019-09-04 22:41:48 +0100

    Atualização do layout da navbar comum a todas as páginas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'KOMPARE') }}
+                    KOMPARE
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -32,7 +32,12 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                   
                     <ul class="navbar-nav mr-auto">
+                        <form action="produtos/index.blade.php" method="get">
+                                <input type="text" name="nome" id="idNome" placeholder="Produto a pesquisar" value="">
+                                <input type="submit" value="Pesquisar" class="btn btn-search">
+                        </form>
 
                     </ul>
 
@@ -49,6 +54,11 @@
                                 </li>
                             @endif
                         @else
+                        <li class="nav-item">
+                                <a class="nav-link" href="{{ route('produtos.index') }}">{{ __('Produtos') }}</a>
+                            </li>
+                        <li class="nav-item">
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
